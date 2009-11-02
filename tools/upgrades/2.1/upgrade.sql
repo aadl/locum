0bee35e6ed31aa50b1be2f6e21edd4d7c7149cb4
John Blyberg <john@blyberg.net>
2009-11-02 16:25:48 +0000

    Slight SQL updates
    git-svn-id: http://dobby.darienlibrary.org/svn/locum/trunk@125 52fc8af8-1f85-4cc4-a879-7157b0ac4467

--- tools/upgrades/2.1/upgrade.sql
+++ tools/upgrades/2.1/upgrade.sql
@@ -8,21 +8,18 @@ ALTER TABLE locum_holds_placed CONVERT TO CHARACTER SET utf8 COLLATE utf8_genera
 
 ALTER TABLE `locum_bib_items` ADD `upc` BIGINT UNSIGNED ZEROFILL NOT NULL AFTER `stdnum` ;
 
+ALTER TABLE `insurge_index` CHANGE `bnum` `bnum` INT( 12 ) NOT NULL
+ALTER TABLE `locum_bib_items` CHANGE `bnum` `bnum` INT( 12 ) NOT NULL
+ALTER TABLE `locum_bib_items_subject` CHANGE `bnum` `bnum` INT( 12 ) NOT NULL
+ALTER TABLE `locum_facet_heap` CHANGE `bnum` `bnum` INT( 12 ) NOT NULL
+
 CREATE TABLE IF NOT EXISTS `locum_availability` (
-  `bnum` mediumint(8) unsigned NOT NULL,
+  `bnum` int(12) unsigned NOT NULL,
   `ages` varchar(128) NOT NULL,
   `locations` varchar(128) NOT NULL,
   `available` blob NOT NULL,
   `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`bnum`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `locum_inum_to_bnum` (
- `inum` int(10) unsigned NOT NULL,
- `bnum` int(10) unsigned NOT NULL,
- `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update
-CURRENT_TIMESTAMP,
- PRIMARY KEY  (`inum`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `locum_syndetics_links` (
