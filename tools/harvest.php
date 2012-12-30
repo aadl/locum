0f1929ff7aa9d4429fbabb0a2cb261c7c92dc96f
Ryan Eby <ryaneby@gmail.com>
2012-12-30 14:19:30 -0500

    limit fiction fix to keyword for now. messing up things like science fiction callnum search

--- tools/harvest.php
+++ tools/harvest.php
@@ -3,7 +3,7 @@
 
 // You'll need to change these:
 $first_record = 1000000;
-$last_record = 1420000;
+$last_record = 1430000;
 $large_record_split = 50;
 
 // Init scripts, library locations, and binaries
